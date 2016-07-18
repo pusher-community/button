37974895e762fbc16299ca4fe01bda05fc6c9c64
Ben Foxall <ben@pusher.com>
2016-07-19 00:34:34 +0200

    Revert "hardcode"
    This reverts commit d4b51f07e797db42fb0ced56f991da7f01d737c0.

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ const Pusher = require('pusher')
 const pusher = new Pusher({
   appId:   process.env.PUSHER_APP_ID,
   key:     process.env.PUSHER_APP_KEY,
-  secret:  process.env.PUSHER_SECRET_KEY//,
-  //cluster: process.env.PUSHER_CLUSTER
+  secret:  process.env.PUSHER_SECRET_KEY,
+  cluster: process.env.PUSHER_CLUSTER
 })
 
 
